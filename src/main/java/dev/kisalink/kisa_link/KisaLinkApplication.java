7d36de5a7571dbbff4e03132b761daf044d9a5f1
OsmanBeratPasa <osmanberatpasa@gmail.com>
2026-09-21 13:58:04 +0300

    kisa-link iskeleti ve /hello uç noktası

---
+++ src/main/java/dev/kisalink/kisa_link/KisaLinkApplication.java
@@ -0,0 +1,13 @@
+package dev.kisalink.kisa_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisaLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisaLinkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/kisalink/kisa_link/web/HelloController.java
@@ -0,0 +1,16 @@
+package dev.kisalink.kisa_link.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController{
+
+@GetMapping( "/hello")
+
+public String hello(){
+
+return "kısa-link-ayakta";}
+}
+
+
