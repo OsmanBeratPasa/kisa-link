77783fa2ea14752cd08f1f8e319e805f8ad5d60a
OsmanBeratPasa <osmanberatpasa@gmail.com>
2026-09-23 11:39:11 +0300

    G1.2: service katmanı ve kurucu enjeksiyonu

---
+++ src/main/java/dev/kisalink/kisa_link/service/LinkService.java
@@ -0,0 +1,11 @@
+package dev.kisalink.kisa_link.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LinkService {
+
+    public String selamla() {
+        return "servisten geldi";
+    }
+}
--- src/main/java/dev/kisalink/kisa_link/web/HelloController.java
+++ src/main/java/dev/kisalink/kisa_link/web/HelloController.java
@@ -1,16 +1,20 @@
 package dev.kisalink.kisa_link.web;
 
+import dev.kisalink.kisa_link.service.LinkService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController{
+public class HelloController {
 
-@GetMapping( "/hello")
+    private final LinkService linkService;
 
-public String hello(){
+    public HelloController(LinkService linkService) {
+        this.linkService = linkService;
+    }
 
-return "kısa-link-ayakta";}
+    @GetMapping("/hello")
+    public String hello() {
+        return linkService.selamla();
+    }
 }
-
-
